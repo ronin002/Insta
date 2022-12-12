b0187f22c571bf8a336c3373bbfac7a10ef8d317
Edson Márcio <edson.marcio7@gmail.com>
2022-12-12 18:41:04 -0300

    GitIgnore2

--- backend/controllers/PhotoController.js
+++ backend/controllers/PhotoController.js
@@ -1,4 +1,5 @@
 const Photo = require("../models/Photo")
+const User = require("../models/User")
 
 const mongoose = require("mongoose")
 
@@ -8,9 +9,27 @@ const insertPhoto = async (req,res) => {
     const{title} = req.body;
     const image = req.file.filename;
 
-    console.log(req.body);
+    const reqUser = req.user;
 
-    res.send("Photo Inserted");
+    const user = await User.findById(reqUser._id);
+
+    //Create Photo
+    const newPhoto = await Photo.create({
+        image,
+        title,
+        userId: user._id,
+        userName: user.name,
+    })
+
+    // If photo was created
+
+    if(!newPhoto){
+        res.status(422).json({errors:["Error: PX 001 - Please try again later"]});
+    }
+    res.status(201).json(newPhoto);
+    // console.log(req.body);
+
+    // res.send("Photo Inserted");
 }
 
 module.exports = {insertPhoto}
